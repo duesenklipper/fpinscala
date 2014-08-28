9f1a6183dc7928495e9bbce4d0bd5311829641e3
Carl-Eric Menzel <cmenzel@wicketbuch.de>
2014-08-28 17:49:15 +0200

    ch6 ex 4

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -30,17 +30,42 @@ object RNG {
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (i1, r1) = nonNegativeInt(rng)
+    val (i2, r2) = nonNegativeInt(r1)
+    if (i1 < i2) (i1.toDouble / i2, r2) else (i2.toDouble / i1, r2)
+  }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (i, r1) = nonNegativeInt(rng)
+    val (d, r2) = double(r1)
+    ((i, d), r2)
+  }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
+
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = if (count <= 0) (Nil, rng) else {
+    val (i, r) = rng.nextInt
+    val (tail, r2) = ints(count - 1)(r)
+    (i :: tail, r2)
+  }
 
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
---
+++ exercises/src/test/scala/chap6test.scala
@@ -0,0 +1,27 @@
+package fpinscala.state
+
+import org.scalatest._
+import RNG._
+
+class StateSpec extends FlatSpec with Matchers {
+  "nonNegativeInt" should "never give negative" in {
+    val (rand, rng) = nonNegativeInt(Simple(System.currentTimeMillis()))
+    assert(rand > 0)
+  }
+  
+  "nonNegativeInt" should "be repeatable with same seed" in {
+    val rng: RNG = Simple(System.currentTimeMillis())
+    val (r1, nextRng) = rng.nextInt
+    val (r2, nextRng2) = rng.nextInt
+    assert(r1 == r2)
+    assert(nextRng == nextRng2)
+  }
+  
+  "double" should "give doubles in [0,1)" in {
+    val (d, rng) = double(Simple(System.currentTimeMillis()))
+    assert(d >= 0.0)
+    assert(d < 1.0)
+  }
+  
+  
+}
